a32d8e970965b5a6e9d9d9cdb80e3bdeebbfd4f6
WinCodeX <glenwinterg970@gmail.com>
2023-02-13 17:02:28 +0300

    articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,6 @@
-class CreateArticles < ActiveRecord::Migration[7.0]
-    def change
-      create_table :articles do |t|
-        t.string :title
-        t.text :body
-  
-        t.timestamps
-      end
-    end
-  end
+class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+end
+
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-    root "articles#index"
-  
-    get "/articles", to: "articles#index"
-    get "/articles/:id", to: "articles#show"
-    resources :articles
-  end
+  root "articles#index"
+
+  resources :articles
+end
+
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
